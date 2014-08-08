2dcfb18e47765aec319c03b57312816faa2bb517
David G. López <guitarrkurt@icloud.com>
2014-08-07 19:01:15 -0500

    Posicionamos arriba a la izquierda para cualquier resolucion de pantalla

--- Rack/AppDelegate.m
+++ Rack/AppDelegate.m
@@ -11,13 +11,24 @@
 
 @implementation AppDelegate
 
-
-
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    NSColor *color =[NSColor colorWithDeviceRed:1 green:1 blue:1 alpha:.6];
+    NSColor *color =[NSColor colorWithDeviceRed:1 green:1 blue:1 alpha:.1];
     [_window setOpaque:NO];
     [_window setBackgroundColor:color];
+    
+    //Creamos un punto que marcará la nueva posición
+    NSPoint posicion;
+    posicion.x = 0;
+    posicion.y = 0;
+    
+    //Obtenemos los bordes de nuestra pantalla y se los asignamos al punto
+    //posicion.x = _window.screen.frame.size.height;
+    posicion.x = 0;
+    posicion.y = _window.screen.frame.size.width;
+
+    //Modificamos el inicio de la ventana que sea arriba a la izquierda
+    [_window setFrameTopLeftPoint:posicion];
 }
 
 - (IBAction)SubVentana:(id)sender {
