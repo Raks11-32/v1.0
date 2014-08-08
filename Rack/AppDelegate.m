8609d0b3a11cef94cac6ccca61f5361b3671d058
David G. López <guitarrkurt@icloud.com>
2014-08-07 23:44:44 -0500

    Agregamos el blur a la ventana

--- Rack/AppDelegate.m
+++ Rack/AppDelegate.m
@@ -18,7 +18,7 @@
     NSColor *color =[NSColor colorWithDeviceRed:0.0f green:0.0f blue:0.0f alpha:0.7f];
     [_window setOpaque:NO];
     [_window setBackgroundColor:color];
-    
+
     /*Cambiamos el tamaño*/
     float anchoWindow = _window.screen.frame.size.width;
     float altoWindow = _window.screen.frame.size.height;
@@ -36,7 +36,7 @@
 
     //Modificamos el inicio de la ventana que sea arriba a la izquierda
     [_window setFrameTopLeftPoint:posicion];
-    
+
     /*Agregar el label (customView) en la ventana*/
     //Apartir de la funcion "labelColor" creamos el tamaño de una etiqueta y se la mandamos para que la pinte
     labelColorView *label = [[labelColorView alloc] initWithFrame:NSMakeRect(0.0f, altoWindow-210, anchoWindow, 80.0f)];
@@ -45,6 +45,9 @@
     //Agregamos el label al contenedor de vistas
     [contentView addSubview:label];
     //Nos basamos en el video www.youtube.com/watch?v=K8eWIZ39W4A
+    
+
+    [self enableBlurForWindow:self.window];
     
 }
 
@@ -57,5 +60,19 @@
     
     [segundaventana showWindow:self];
     
+}
+//cocoasnippets.io/cocoa/ui/nswindow/coregraphics/2014/02/27/translucent-nswindow-with-blurred-background.html
+//stackoverflow.com/questions/11174918/how-to-get-window-with-semi-transparent-blurred-background
+typedef void * CGSConnection;
+extern OSStatus CGSSetWindowBackgroundBlurRadius(CGSConnection connection, NSInteger   windowNumber, int radius);
+extern CGSConnection CGSDefaultConnectionForThread();
+
+- (void)enableBlurForWindow:(NSWindow *)window
+{
+    [window setOpaque:NO];
+    window.backgroundColor = [NSColor colorWithDeviceRed:0.0f green:0.0f blue:0.0f alpha:0.7f];
+    
+    CGSConnection connection = CGSDefaultConnectionForThread();
+    CGSSetWindowBackgroundBlurRadius(connection, [window windowNumber], 50);
 }
 @end
