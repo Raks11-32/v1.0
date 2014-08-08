75f9d5bd850bca9d808d276edbe8c4eec5de999c
David G. López <guitarrkurt@icloud.com>
2014-08-07 19:42:32 -0500

    Ajustamos la ventana al tamaño de cualquier pantalla, pantalla = [screen bounds]

--- Rack/AppDelegate.m
+++ Rack/AppDelegate.m
@@ -13,22 +13,29 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+    /*Color de fondo*/
     NSColor *color =[NSColor colorWithDeviceRed:0.0f green:0.0f blue:0.0f alpha:0.7f];
     [_window setOpaque:NO];
     [_window setBackgroundColor:color];
     
+    /*Cambiamos el tamaño*/
+    float anchoWindow = _window.screen.frame.size.width;
+    float altoWindow = _window.screen.frame.size.height;
+    [_window setFrame:NSMakeRect(0.f, 0.f, anchoWindow, altoWindow) display:YES animate:YES];
+    
+    /*Posicionamos Ventana*/
     //Creamos un punto que marcará la nueva posición
     NSPoint posicion;
     posicion.x = 0;
     posicion.y = 0;
     
     //Obtenemos los bordes de nuestra pantalla y se los asignamos al punto
-    //posicion.x = _window.screen.frame.size.height;
     posicion.x = 0;
-    posicion.y = _window.screen.frame.size.width;
+    posicion.y = altoWindow;
 
     //Modificamos el inicio de la ventana que sea arriba a la izquierda
     [_window setFrameTopLeftPoint:posicion];
+
 }
 
 - (IBAction)SubVentana:(id)sender {
